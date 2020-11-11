8b7b696a6479ce5d12593ee79ea2edefbc125b46
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2020-11-11 15:19:08 -0500

    fix(test): resolve pytest exiting code 5, failing Travis builds

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,10 +1,20 @@
+from copy import copy
+
 from _pytest.python import _showfixtures_main
 
 
 def test_pytest_fixtures(request):
     """Running `py.test --fixtures` should not fail
     """
+    #
+    # Use a copy of the session, so as not to interfere with collection stats.
+    #
+    # If the actual session are is used, pytest will exit with code 5:
+    # no tests collected. This causes the Travis build to be marked as failed.
+    #
+    session = copy(request.session)
+
     try:
-        _showfixtures_main(request.config, request.session)
+        _showfixtures_main(request.config, session)
     except Exception as e:
         raise AssertionError(f'Error showing fixtures: {e}')
